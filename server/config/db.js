c02bdeaba44884398e1c37e01f95ab7d32a4b4c8
Dhrumit1677 <dhrumit.patel1677@gmail.com>
2022-10-27 09:14:13 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  // "URI": "mongodb://localhost/book_store",
+  "URI": "mongodb+srv://Dhrumit:BbFJKmbL2ASV6OB2@midtermcluster.45pfxik.mongodb.net/?retryWrites=true&w=majority",
+  "Secret": "SomeSecret"
+};
